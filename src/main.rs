46bd459f3473faad758706553024f77b98f9cf70
r-preston <rowan.t.preston@gmail.com>
2024-09-15 18:27:26 +0100

    Rough structure

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,12 @@ fn main() {
 fish - flocking, try and stay in sphere, avoid predators
 sharks - roam near prey, sometimes attack
 world - hold list of actors, passes data to renderer
-renderer - track timer and draw things
+renderer (main) - track timer and draw things
 camera - moveable
+
+// workflow:
+have an array of fish positions
+create a density map from fish positions each tick
+update all fish with using this density map
+update predators via density map
+multiple density maps? fish vs threat
