d66c7a64c7cbc59f3f611aa1d41da61b794d2d5e
r-preston <rowan.t.preston@gmail.com>
2024-09-15 18:08:24 +0100

    Add dependency wgpu

--- src/main.rs
+++ src/main.rs
@@ -2,3 +2,9 @@ fn main() {
     println!("Hello, world!");
 }
 
+// things I will need
+fish - flocking, try and stay in sphere, avoid predators
+sharks - roam near prey, sometimes attack
+world - hold list of actors, passes data to renderer
+renderer - track timer and draw things
+camera - moveable
