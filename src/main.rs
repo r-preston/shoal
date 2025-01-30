90b571f25b9a1010de55fc742a04efef2bd5a10b
Rowan <rowan.t.preston@gmail.com>
2025-01-30 20:56:29 +0000

    restructuring around render loop

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
+#![allow(dead_code)]
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+
 mod actors;
-mod camera;
 mod world;
 mod utility;
 mod renderer;
 
+use winit::{
+    event::*,
+    event_loop::EventLoop,
+    keyboard::{KeyCode, PhysicalKey},
+    window::WindowBuilder,
+};
 
-fn main() {
-    let _world = world::World::new(100.0, 5, 10, 0);
-    renderer::run();    
-}
+use renderer::Renderer;
+use world::World;
 
 /*
 // things I will need
@@ -25,3 +32,87 @@ update all fish with using these maps
 update predators using maps
 multiple density maps? fish vs threat
 */
+fn main() {
+    let mut state = State::new(
+        World::new(100.0, 5, 10, 0)
+    );
+    pollster::block_on(state.run());
+}
+
+struct State {
+    world: World
+}
+
+impl State {
+
+pub fn new(world: World) -> State {
+    Self {
+        world
+    }
+}
+
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
+pub async fn run(&mut self) {
+    env_logger::init();
+
+    let event_loop = EventLoop::new().unwrap();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+    let mut renderer = Renderer::new(&window).await;
+
+    let _ = event_loop.run(move |event, control_flow| match event {
+        Event::WindowEvent {
+            ref event,
+            window_id,
+        } if window_id == renderer.window().id() => if !renderer.input(event) { 
+            match event {
+
+                WindowEvent::CloseRequested
+                | WindowEvent::KeyboardInput {
+                    event:
+                        KeyEvent {
+                            state: ElementState::Pressed,
+                            physical_key: PhysicalKey::Code(KeyCode::Escape),
+                            ..
+                        },
+                    ..
+                } => control_flow.exit(),
+
+                WindowEvent::Resized(physical_size) => {
+                    renderer.resize(*physical_size);
+                },
+                
+                WindowEvent::RedrawRequested => {
+                    // This tells winit that we want another frame after this one
+                    renderer.window().request_redraw();
+        
+                    renderer.update();
+
+                    match renderer.render() {
+                        Ok(_) => {}
+
+                        // Reconfigure the surface if it's lost or outdated
+                        Err(
+                            wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated,
+                        ) => renderer.resize(renderer.size()),
+
+                        // The system is out of memory, we should probably quit
+                        Err(wgpu::SurfaceError::OutOfMemory) => {
+                            log::error!("OutOfMemory");
+                            control_flow.exit();
+                        }
+        
+                        // This happens when the a frame takes too long to present
+                        Err(wgpu::SurfaceError::Timeout) => {
+                            log::warn!("Surface timeout")
+                        }
+                    }
+                },
+
+                _ => {}
+            }
+        },
+        _ => {}
+    });
+}
+
+}
