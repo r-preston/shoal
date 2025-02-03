423df66d051bdf782d49117308eeaa53d06b2976
Rowan <rowan.t.preston@gmail.com>
2025-02-03 18:16:49 +0000

    clamp to 60fps

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,18 @@ mod renderer;
 mod utility;
 mod world;
 
+use crate::utility::*;
+use renderer::{camera::Camera, Renderer};
+use std::{
+    alloc::System,
+    time::{Duration, SystemTime},
+};
 use winit::{
     event::*,
-    event_loop::EventLoop,
+    event_loop::{ControlFlow, EventLoop},
     keyboard::{KeyCode, PhysicalKey},
     window::WindowBuilder,
 };
-
-use crate::utility::Position;
-use renderer::{camera::Camera, Renderer};
 use world::World;
 
 /*
@@ -34,19 +37,37 @@ update predators using maps
 multiple density maps? fish vs threat
 */
 fn main() {
+    let start = SystemTime::now();
+
     let mut state = State::new(World::new(100.0, 5, 10, 0));
     pollster::block_on(state.run());
+
+    let end = SystemTime::now();
+    println!(
+        "{} frames in {}ms",
+        state.frame(),
+        SystemTime::now().duration_since(start).unwrap().as_millis()
+    );
 }
 
 struct State {
     world: World,
     camera: Camera,
+    frame: u32,
 }
 
 impl State {
     pub fn new(world: World) -> State {
         let camera = Camera::new(1.3 * world.size());
-        Self { world, camera }
+        Self {
+            world,
+            camera,
+            frame: 0,
+        }
+    }
+
+    pub fn frame(&self) -> u32 {
+        self.frame
     }
 
     #[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
@@ -57,60 +78,92 @@ impl State {
         let window = WindowBuilder::new().build(&event_loop).unwrap();
         let mut renderer = Renderer::new(&window).await;
 
-        let _ = event_loop.run(move |event, control_flow| match event {
-            Event::WindowEvent {
-                ref event,
-                window_id,
-            } if window_id == renderer.window().id() => {
-                if !renderer.input(event) {
-                    match event {
-                        WindowEvent::CloseRequested
-                        | WindowEvent::KeyboardInput {
-                            event:
+        let perspective = cgmath::PerspectiveFov::<f32> {
+            fovy: Radians::from(Degrees(90.0)),
+            aspect: (window.inner_size().width as f32) / (window.inner_size().height as f32),
+            near: 1.0,
+            far: self.world.size() * 3.0,
+        };
+        let perspective_matrix = Mat4::from(perspective);
+
+        let mut last_frame = SystemTime::now();
+        let _ = event_loop.run(move |event, control_flow| {
+            match event {
+                Event::WindowEvent {
+                    ref event,
+                    window_id,
+                } if window_id == renderer.window().id() => {
+                    if !renderer.input(event) {
+                        match event {
+                            WindowEvent::CloseRequested
+                            | WindowEvent::KeyboardInput {
+                                event:
+                                    KeyEvent {
+                                        state: ElementState::Pressed,
+                                        physical_key: PhysicalKey::Code(KeyCode::Escape),
+                                        ..
+                                    },
+                                ..
+                            } => control_flow.exit(),
+                            /*
+                            WindowEvent::CursorMoved {
+                                position: mouse_position, ..
+                            } => match key_event {
                                 KeyEvent {
                                     state: ElementState::Pressed,
-                                    physical_key: PhysicalKey::Code(KeyCode::Escape),
+                                    physical_key: PhysicalKey::Code(KeyCode::KeyW),
                                     ..
-                                },
-                            ..
-                        } => control_flow.exit(),
+                                } => {println!("W");}
+                                _ => {}
+                            },
+                            */
+                            WindowEvent::Resized(physical_size) => {
+                                renderer.resize(*physical_size);
+                            }
 
-                        WindowEvent::Resized(physical_size) => {
-                            renderer.resize(*physical_size);
-                        }
+                            WindowEvent::RedrawRequested => 'RedrawLabel: {
+                                renderer.window().request_redraw();
+                                //println!("{}", SystemTime::now().duration_since(last_frame).unwrap().as_millis());
 
-                        WindowEvent::RedrawRequested => {
-                            // This tells winit that we want another frame after this one
-                            renderer.window().request_redraw();
-
-                            renderer.update();
-
-                            match renderer.render() {
-                                Ok(_) => {}
-
-                                // Reconfigure the surface if it's lost or outdated
-                                Err(wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated) => {
-                                    renderer.resize(renderer.size())
+                                if SystemTime::now().duration_since(last_frame).unwrap()
+                                    < Duration::from_nanos(16666667)
+                                {
+                                    break 'RedrawLabel;
                                 }
 
-                                // The system is out of memory, we should probably quit
-                                Err(wgpu::SurfaceError::OutOfMemory) => {
-                                    log::error!("OutOfMemory");
-                                    control_flow.exit();
-                                }
+                                println!("{}", self.frame);
+                                self.frame += 1;
+                                last_frame = SystemTime::now();
 
-                                // This happens when the a frame takes too long to present
-                                Err(wgpu::SurfaceError::Timeout) => {
-                                    log::warn!("Surface timeout")
+                                renderer.update();
+
+                                match renderer.render() {
+                                    Ok(_) => {}
+
+                                    // Reconfigure the surface if it's lost or outdated
+                                    Err(
+                                        wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated,
+                                    ) => renderer.resize(renderer.size()),
+
+                                    // The system is out of memory, we should probably quit
+                                    Err(wgpu::SurfaceError::OutOfMemory) => {
+                                        log::error!("OutOfMemory");
+                                        control_flow.exit();
+                                    }
+
+                                    // This happens when the a frame takes too long to present
+                                    Err(wgpu::SurfaceError::Timeout) => {
+                                        log::warn!("Surface timeout")
+                                    }
                                 }
                             }
-                        }
 
-                        _ => {}
+                            _ => {}
+                        }
                     }
                 }
+                _ => {}
             }
-            _ => {}
         });
     }
 }
--- src/renderer/camera.rs
+++ src/renderer/camera.rs
@@ -6,7 +6,6 @@ pub struct Camera {
     distance: f32,
     azimuthal_angle: Radians,
     polar_angle: Radians,
-    perspective_matrix: Mat4,
 }
 
 impl Camera {
@@ -26,13 +25,22 @@ impl Camera {
         Position::new(0.0, 0.0, 0.0)
     }
 
+    pub fn new(distance: f32) -> Camera {
+        Self {
+            target: Camera::default_target(),
+            distance,
+            azimuthal_angle: Radians(0.0),
+            polar_angle: Radians(0.0),
+        }
+    }
+
     pub fn position(&self) -> Position {
         let (sin_theta, cos_theta) = self.polar_angle.sin_cos();
         let (sin_phi, cos_phi) = self.azimuthal_angle.sin_cos();
         self.distance * Position::new(cos_theta * cos_phi, cos_theta * sin_phi, sin_theta)
     }
 
-    fn generate_view_matrix(&self) -> Mat4 {
+    pub fn view_matrix(&self) -> Mat4 {
         let position = self.position();
         let direction = Direction::from(self.position() - self.target).normalize();
         let right = Camera::global_up().cross(direction);
@@ -40,24 +48,8 @@ impl Camera {
         return Mat4::look_at_rh(position, self.target, up);
     }
 
-    pub fn new(distance: f32) -> Camera {
-        let perspective = cgmath::PerspectiveFov::<f32> {
-            fovy: Radians::from(Degrees(90.0)),
-            aspect: 1.0,
-            near: 0.0,
-            far: 100.0,
-        };
-        Self {
-            target: Camera::default_target(),
-            distance,
-            azimuthal_angle: Radians(0.0),
-            polar_angle: Radians(0.0),
-            perspective_matrix: Mat4::from(perspective),
-        }
-    }
-
     pub fn move_in_out(&mut self, amount: f32) {
-        self.distance += (amount * Camera::radial_sensitivity());
+        self.distance += amount * Camera::radial_sensitivity();
         self.distance = f32::max(1.0, self.distance);
     }
 
@@ -73,9 +65,5 @@ impl Camera {
     pub fn move_left_right(&mut self, amount: f32) {
         self.azimuthal_angle += Radians(amount * Camera::angular_sensitivity());
         self.azimuthal_angle = self.azimuthal_angle.normalize();
-    }
-
-    pub fn vp_matrix(&self) -> Mat4 {
-        self.generate_view_matrix() * self.perspective_matrix
     }
 }
